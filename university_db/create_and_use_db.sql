cbc83ed6056d9f7557d3ce6e1513cab59af5766a
jamesenoviso <159095733+jamesenoviso@users.noreply.github.com>
2025-02-16 13:28:56 +0800

    create_and_use_db.sql

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -1,7 +1,5 @@
 -- Step 1: Create a database called UniversityDB
 CREATE DATABASE `UniversityDB`;
-
 -- Step 2: Use the newly created database
 USE `UniversityDB`;
-
-SELECT DATABASE(); -- This will show the currently selected database.
+SELECT DATABASE();-- This will show the currently selected database.
