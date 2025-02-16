f49294789c9cddfdb6e71b94b0e1ad00f7611040
jamesenoviso <159095733+jamesenoviso@users.noreply.github.com>
2025-02-16 13:11:40 +0800

    create_and_use_db.sql

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -4,14 +4,4 @@ CREATE DATABASE `UniversityDB`;
 -- Step 2: Use the newly created database
 USE `UniversityDB`;
 
-CREATE TABLE Students (
-    StudentID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(100),
-    Age INT
-);
-
-INSERT INTO Students (Name, Age) VALUES 
-('Alice', 20), 
-('Bob', 22);
-
 SELECT DATABASE(); -- This will show the currently selected database.
